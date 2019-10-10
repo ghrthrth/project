a620fbc26e5d5e5b30ecac0e77228f4f3ac5c4c5
ghrthrth <50299852+ghrthrth@users.noreply.github.com>
2019-10-10 17:45:56 +0300

    Add files via upload

---
+++ script.php
@@ -0,0 +1,29 @@
+﻿<?php
+header('Access-Control-Allow-Origin: *');
+$mysql_host = "www.zzz.com";
+$mysql_database = "dimasik2283030";
+$mysql_user = "dimasik2283030";
+$mysql_password = "вафля303";
+$link = mysql_connect($mysql_host, $mysql_user, $mysql_password) or die("Ошибка при подключении MySQL" );
+mysql_select_db($mysql_database, $link) or die ('Ошибка при подключении к БД');
+if(isset($_POST['name'])) $name = $_POST['name'];
+if(isset($_POST['score'])) $score = $_POST['score'];
+if(isset($name) && isset($score)) {
+$q1 = mysql_query("SELECT * FROM `result_table` WHERE `name`='".$name."'");
+if(mysql_num_rows($q1) == 1) {
+$array = mysql_fetch_array($q1);
+if($score > $array['score']) $q3 = mysql_query("UPDATE `result_table` SET `score`='".$score."' WHERE `name`='".$name."'");
+}
+else 
+$q2 = mysql_query("INSERT INTO `result_table`(`name`, `score`) VALUES ('".$name."', '".$score."')");
+}
+$q4 = mysql_query("SELECT * FROM `result_table` ORDER BY `score` DESC");
+$i=0;
+while($row = mysql_fetch_row($q4)){
+
+    if($i<10) {
+	echo $row[0].' - '.$row[1].'|';
+	$i=$i+1;
+	}
+}
+?>
